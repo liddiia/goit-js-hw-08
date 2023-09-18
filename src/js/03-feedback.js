1eb9d63174cf50ee0106e128bccad0884b3a6bbe
liddiia <liddabojko@gmail.com>
2023-09-19 01:59:10 +0300

    form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,36 @@
 import throttle from 'lodash.throttle';
 
-const Email = document.querySelector()
+const formRef = document.querySelector('.feedback-form');
 
-localStorage.setItem()
+let objOfValue = JSON.parse(localStorage.getItem('feedback-form-state')) ?? {
+        email: '',
+        message: ''
+}
+    
+const { email, message } = formRef.elements;
+
+email.value = objOfValue.email.trim();
+message.value = objOfValue.message.trim();
+
+formRef.addEventListener('input', throttle(onInputForm, 500));
+formRef.addEventListener('submit', onSubmitForm);
+
+function onInputForm(event) {
+    objOfValue = {
+        email: email.value.trim(),
+        message: message.value.trim()
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(objOfValue));
+}
+
+function onSubmitForm(event) {
+    event.preventDefault();
+    localStorage.removeItem('feedback-form-state');
+
+    if (email.value === "" || message.value === "") {
+    return alert("Fill in all the fields, please!");
+  }
+    console.log("User information:\n", objOfValue);
+
+    event.currentTarget.reset();
+}
